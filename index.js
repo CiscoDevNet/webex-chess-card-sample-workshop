e1316af57577f771a6606bc9247b1d25113434a2
dstaudt <dstaudt@cisco.com>
2021-03-07 15:55:12 -0600

    Remove default message handling

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ var config = {
 var framework = new Framework(config);
 framework.start();
 
-var responded = false;
-
 framework.hears('hello', (bot, trigger) => {
     bot.say(`Hello ${trigger.person.displayName}!  Say "new" to start a new game of chess`,);
-    responded = true;
 });
