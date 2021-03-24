c3ca399744858435d1236e2eea4601b847950d66
dstaudt <dstaudt@cisco.com>
2021-03-24 12:35:03 -0500

    Fix require for chess.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var Framework = require('webex-node-bot-framework');
 const fetch = require('node-fetch');
 require('dotenv').config();
-var chess = require('./chess');
 
 var config = {
     token: process.env.WEBEX_ACCESS_TOKEN
@@ -14,6 +13,7 @@ framework.hears('hello', (bot, trigger) => {
     bot.say(`Hello ${trigger.person.displayName}!  Say "new" to start a new game of chess`,);
 });
 
+var chess = require('./chess');
 framework.hears('new', (bot, trigger) => {
     bot.sendCard(chess.start(), "Sorry, it appears your client cannot render adaptive card attachments");
 });
